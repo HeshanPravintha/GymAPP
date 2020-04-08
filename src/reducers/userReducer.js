2d03705878075bd449455182872d4a222930f061
Shafran <Shafran@ieee.org>
2020-04-08 22:22:57 +0530

    adde redux

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,13 @@
+/* eslint-disable semi */
+/* eslint-disable comma-dangle */
+/* eslint-disable prettier/prettier */
+
+const INITIAL_STATE = { redux: [] }
+
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+      
+        default:
+            return state;
+    }
+}
